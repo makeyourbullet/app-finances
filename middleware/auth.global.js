085e6a364fbec25a6f2a6b497fa4374e6cc83e06
makeyourbullet <contact@makeyourbullet.com>
2025-07-10 12:53:53 +0200

    Garder la connexion au refresh

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,4 +1,6 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
+  if (process.server) return // Ne rien faire côté SSR
+
   const client = useSupabaseClient()
   const { data } = await client.auth.getSession()
   if (!data.session && to.path !== '/login') {
